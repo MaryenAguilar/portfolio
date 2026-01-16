4ed02cc2c26c0488d3e6b7a0eb64874789602c79
MaryenAguilar <maryenaguilarzuniga.com>
2026-01-16 18:00:31 -0500

    Fix: adjust base path for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'https://maryenaguilar.github.io/portfolio/',
+  base: '/portfolio/',
 })
